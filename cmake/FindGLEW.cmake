dd677db67146c2cb9a00c907a5ff6c299fb0aa82
José Fonseca <jfonseca@vmware.com>
2014-05-29 12:51:45 +0100

    cmake: Output a message when GLEW is found.

--- cmake/FindGLEW.cmake
+++ cmake/FindGLEW.cmake
@@ -3,6 +3,7 @@ find_library (GLEW_glew_LIBRARY GLEW)
 
 if (GLEW_INCLUDE_DIR AND GLEW_glew_LIBRARY)
     set (GLEW_FOUND 1)
+    find_package_message(GLEW "Found GLEW" "[${GLEW_glew_LIBRARY}][${GLEW_INCLUDE_DIR}]")
 endif ()
 
 mark_as_advanced (GLEW_FOUND)
