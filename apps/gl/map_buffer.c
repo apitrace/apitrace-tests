2a5d154be7f179245c793a47400d0579e392bf64
José Fonseca <jfonseca@vmware.com>
2014-06-03 19:49:54 +0100

    apps/gl: Fix creation of GL 3.2 context on Mac.

--- apps/gl/map_buffer.c
+++ apps/gl/map_buffer.c
@@ -212,6 +212,7 @@ int main(int argc, char** argv)
     if (mapMethod == MAP_BUFFER_RANGE) {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     }
 #endif
