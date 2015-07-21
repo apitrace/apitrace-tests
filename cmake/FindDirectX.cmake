1452b0333b3103bf0457c2c91148a7f3e173b077
Jose Fonseca <jfonseca@vmware.com>
2015-07-21 13:39:12 +0100

    cmake: Simplify DirectX search.

--- cmake/FindDirectX.cmake
+++ cmake/FindDirectX.cmake
@@ -79,60 +79,46 @@ if (WIN32)
     endif ()
 
     # Find a header in the DirectX SDK
-    macro (find_dxsdk_header var_name header)
+    macro (find_dx_header    var_name header)
         set (include_dir_var "DirectX_${var_name}_INCLUDE_DIR")
         set (include_found_var "DirectX_${var_name}_INCLUDE_FOUND")
-        find_path (${include_dir_var} ${header}
-            HINTS ${DirectX_INC_SEARCH_PATH}
-            DOC "The directory where ${header} resides"
-            CMAKE_FIND_ROOT_PATH_BOTH
-        )
-        if (${include_dir_var})
-            set (${include_found_var} TRUE)
-            find_package_message (${var_name}_INC "Found ${header} header: ${${include_dir_var}}/${header}" "[${${include_dir_var}}]")
+        set (${include_dir_var})
+
+        # Search in the Windows SDK
+        check_include_file_cxx (${header} ${include_found_var})
+
+        if (NOT ${include_found_var})
+            # Search in the DirectX SDK
+            find_path (${include_dir_var} ${header}
+                HINTS ${DirectX_INC_SEARCH_PATH}
+                DOC "The directory where ${header} resides"
+                CMAKE_FIND_ROOT_PATH_BOTH
+            )
+            if (${include_dir_var})
+                set (${include_found_var} TRUE)
+                find_package_message (${var_name}_INC "Found ${header} header: ${${include_dir_var}}/${header}" "[${${include_dir_var}}]")
+            endif ()
         endif ()
         mark_as_advanced (${include_found_var})
     endmacro ()
 
-    # Find a library in the DirectX SDK
-    macro (find_dxsdk_library var_name library)
-        # DirectX SDK
+    macro (find_dx_library var_name library)
         set (library_var "DirectX_${var_name}_LIBRARY")
+        # Search in the Windows SDK
         find_library (${library_var} ${library}
-            HINTS ${DirectX_LIB_SEARCH_PATH}
-            DOC "The directory where ${library} resides"
+            DOC "Path to ${library} library"
             CMAKE_FIND_ROOT_PATH_BOTH
+        )
+        # Search in the DirectX SDK
+        find_library (${library_var} ${library}
+            PATHS ${DirectX_LIB_SEARCH_PATH}
+            DOC "Path to ${library} library"
+            NO_DEFAULT_PATH
         )
         if (${library_var})
             find_package_message (${var_name}_LIB "Found ${library} library: ${${library_var}}" "[${${library_var}}]")
         endif ()
         mark_as_advanced (${library_var})
-    endmacro ()
-
-    # Find a header in the Windows SDK
-    macro (find_winsdk_header var_name header)
-        if (USE_WINSDK_HEADERS)
-            # Windows SDK
-            set (include_dir_var "DirectX_${var_name}_INCLUDE_DIR")
-            set (include_found_var "DirectX_${var_name}_INCLUDE_FOUND")
-            check_include_file_cxx (${header} ${include_found_var})
-            set (${include_dir_var})
-            mark_as_advanced (${include_found_var})
-        else ()
-            find_dxsdk_header (${var_name} ${header})
-        endif ()
-    endmacro ()
-
-    # Find a library in the Windows SDK
-    macro (find_winsdk_library var_name library)
-        if (USE_WINSDK_HEADERS)
-            # XXX: We currently just assume the library exists
-            set (library_var "DirectX_${var_name}_LIBRARY")
-            set (${library_var} ${library})
-            mark_as_advanced (${library_var})
-        else ()
-            find_dxsdk_library (${var_name} ${library})
-        endif ()
     endmacro ()
 
     # Combine header and library variables into an API found variable
@@ -143,73 +129,68 @@ if (WIN32)
         endif ()
     endmacro ()
 
-    find_winsdk_header  (DDRAW   ddraw.h)
-    find_winsdk_library (DDRAW   ddraw)
+    find_dx_header      (DDRAW   ddraw.h)
+    find_dx_library     (DDRAW   ddraw)
     find_combined       (DDRAW   DDRAW DDRAW)
 
     if (CMAKE_GENERATOR_TOOLSET MATCHES "_xp$")
         set (WINDOWS_XP TRUE)
     endif ()
 
-    if (WINDOWS_XP)
-        # Windows 7 SDKs, used by XP toolset, do not include d3d.h
-        find_dxsdk_header   (D3D     d3d.h)
-    else ()
-        find_winsdk_header  (D3D     d3d.h)
-    endif ()
+    find_dx_header      (D3D     d3d.h)
     find_combined       (D3D     D3D DDRAW)
 
-    find_dxsdk_header   (D3DX    d3dx.h)
+    find_dx_header      (D3DX    d3dx.h)
     find_combined       (D3DX    D3DX D3DX)
 
-    find_dxsdk_header   (D3D8    d3d8.h)
-    find_dxsdk_library  (D3D8    d3d8)
+    find_dx_header      (D3D8    d3d8.h)
+    find_dx_library     (D3D8    d3d8)
     find_combined       (D3D8    D3D8 D3D8)
 
-    find_dxsdk_header   (D3DX8   d3dx8.h)
-    find_dxsdk_library  (D3DX8   d3dx8)
+    find_dx_header      (D3DX8   d3dx8.h)
+    find_dx_library     (D3DX8   d3dx8)
     find_combined       (D3DX8   D3DX8 D3DX8)
 
-    find_winsdk_header  (D3D9    d3d9.h)
-    find_winsdk_library (D3D9    d3d9)
+    find_dx_header      (D3D9    d3d9.h)
+    find_dx_library     (D3D9    d3d9)
     find_combined       (D3D9    D3D9 D3D9)
 
-    find_dxsdk_header   (D3DX9   d3dx9.h)
-    find_dxsdk_library  (D3DX9   d3dx9)
+    find_dx_header      (D3DX9   d3dx9.h)
+    find_dx_library     (D3DX9   d3dx9)
     find_combined       (D3DX9   D3DX9 D3DX9)
 
     if (NOT WINDOWS_XP)
-        find_winsdk_header  (DXGI    dxgi.h)
-        find_winsdk_header  (DXGI1_2 dxgi1_2.h)
-        find_winsdk_header  (DXGI1_3 dxgi1_3.h)
-        find_winsdk_library (DXGI    dxgi)
+        find_dx_header      (DXGI    dxgi.h)
+        find_dx_header      (DXGI1_2 dxgi1_2.h)
+        find_dx_header      (DXGI1_3 dxgi1_3.h)
+        find_dx_library     (DXGI    dxgi)
 
-        find_winsdk_header  (D3D10   d3d10.h)
-        find_winsdk_library (D3D10   d3d10)
+        find_dx_header      (D3D10   d3d10.h)
+        find_dx_library     (D3D10   d3d10)
         find_combined       (D3D10   D3D10 D3D10)
 
-        find_winsdk_header  (D3D10_1 d3d10_1.h)
-        find_winsdk_library (D3D10_1 d3d10_1)
+        find_dx_header      (D3D10_1 d3d10_1.h)
+        find_dx_library     (D3D10_1 d3d10_1)
         find_combined       (D3D10_1 D3D10_1 D3D10_1)
 
-        find_dxsdk_header   (D3DX10  d3dx10.h)
-        find_dxsdk_library  (D3DX10  d3dx10)
+        find_dx_header      (D3DX10  d3dx10.h)
+        find_dx_library     (D3DX10  d3dx10)
         find_combined       (D3DX10  D3DX10 D3DX10)
 
-        find_winsdk_header  (D3D11   d3d11.h)
-        find_winsdk_library (D3D11   d3d11)
+        find_dx_header      (D3D11   d3d11.h)
+        find_dx_library     (D3D11   d3d11)
         find_combined       (D3D11   D3D11 D3D11)
-        find_winsdk_header  (D3D11_1 d3d11_1.h)
+        find_dx_header      (D3D11_1 d3d11_1.h)
         find_combined       (D3D11_1 D3D11_1 D3D11)
-        find_winsdk_header  (D3D11_2 d3d11_2.h)
+        find_dx_header      (D3D11_2 d3d11_2.h)
         find_combined       (D3D11_2 D3D11_2 D3D11)
 
-        find_dxsdk_header   (D3DX11  d3dx11.h)
-        find_dxsdk_library  (D3DX11  d3dx11)
+        find_dx_header      (D3DX11  d3dx11.h)
+        find_dx_library     (D3DX11  d3dx11)
         find_combined       (D3DX11  D3DX11 D3DX11)
 
-        find_winsdk_header  (D2D1    d2d1.h)
-        find_winsdk_library (D2D1    d2d1)
+        find_dx_header      (D2D1    d2d1.h)
+        find_dx_library     (D2D1    d2d1)
         find_combined       (D2D1    D2D1 D2D1)
     endif ()
 
