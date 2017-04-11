4a7b621df18f553879235004ddef58df2027a6b5
Jose Fonseca <jfonseca@vmware.com>
2017-04-11 13:37:14 +0100

    cmake: GLEW is now unused.

--- cmake/FindGLEW.cmake
+++
@@ -1,9 +0,0 @@
-find_path (GLEW_INCLUDE_DIR GL/glew.h)
-find_library (GLEW_glew_LIBRARY GLEW)
-
-if (GLEW_INCLUDE_DIR AND GLEW_glew_LIBRARY)
-    set (GLEW_FOUND 1)
-    find_package_message (GLEW "Found GLEW: ${GLEW_glew_LIBRARY}" "[${GLEW_glew_LIBRARY}][${GLEW_INCLUDE_DIR}]")
-endif ()
-
-mark_as_advanced (GLEW_FOUND)
